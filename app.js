6b414dac8c337f9fed7fbabc8e35c5857b1e0f11
saurav <saurav.kc@gmail.com>
2025-08-22 21:55:23 +0545

    progress

--- app.js
+++ app.js
@@ -2,56 +2,69 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import session from "express-session";
+import { Strategy } from "passport-local";
+import env from "dotenv";
+import passport from "passport";
+import flash from "connect-flash";
 
 const app = express();
 const port = 5000;
+env.config();
+
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "Shape",
-  password: "Ghostrider23@#$",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_host,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.error = req.flash("error");
+  next();
+});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
 app.get("/", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", async (req, res, next) => {
-  let email = req.body.email.trim();
-  let pass = req.body.pass.trim();
-
-  if (!email || !pass) {
-    console.log("not right input");
-    return res.redirect("/");
-  }
-
-  const result = await db.query("SELECT * FROM users where email= $1", [email]);
-  const user = result.rows[0];
-
-  if (!user) {
-    console.log("email not found");
-    return res.redirect("/");
-  }
-
-  const isValid = await bcrypt.compare(pass, user.password);
-  if (isValid) {
-    res.render("home");
-  } else {
-    console.log("false password");
-    res.redirect("/");
-  }
-});
-
-app.get("/test", (req, res) => {
-  res.render("compose");
-});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/",
+    failureFlash: true,
+  })
+);
 
 app.post("/signup", async (req, res) => {
   const fName = req.body.fname.trim();
@@ -60,25 +73,39 @@ app.post("/signup", async (req, res) => {
   const hash = await bcrypt.hash(req.body.pass.trim(), 10);
   const sex = req.body.sex;
 
-  await db.query(
-    "INSERT INTO users(first_name,last_name,email,password,gender) values($1,$2,$3,$4,$5)",
-    [fName, lName, email, hash, sex]
-  );
-  res.redirect("/");
+  try {
+    const result = await db.query("SELECT * FROM users where email= $1", [
+      email,
+    ]);
+    const user = result.rows[0];
+
+    if (user) {
+      console.log("email exist already");
+      return res.redirect("/");
+    }
+
+    await db.query(
+      "INSERT INTO users(first_name,last_name,email,password,gender) values($1,$2,$3,$4,$5)",
+      [fName, lName, email, hash, sex]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.get("/home", async (req, res) => {
+app.get("/home", isAuthenticated, async (req, res) => {
   let result = await db.query("Select * FROM posts ORDER BY created_at DESC");
   let posts = result.rows;
 
   res.render("home", { posts: posts });
 });
 
-app.get("/compose", (req, res) => {
+app.get("/compose", isAuthenticated, (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", async (req, res) => {
+app.post("/compose", isAuthenticated, async (req, res) => {
   const title = req.body.title.trim();
   const detail = req.body.detail.trim();
 
@@ -90,24 +117,64 @@ app.post("/compose", async (req, res) => {
   res.redirect("/compose");
 });
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", isAuthenticated, async (req, res) => {
   let result = await db.query("Select * FROM posts ORDER BY created_at DESC");
   let posts = result.rows;
   res.render("profile", { posts: posts });
 });
 
-app.get("/delete/:id", async (req, res) => {
+app.get("/delete/:id", isAuthenticated, async (req, res) => {
   let id = req.params.id;
   await db.query(`DELETE FROM posts WHERE id=$1`, [id]);
   res.redirect("/profile");
 });
 
-app.post("/update/:id", async (req, res) => {
+app.post("/update/:id", isAuthenticated, async (req, res) => {
   let id = req.params.id;
 
   let detail = req.body.detail.trim();
   await db.query(`UPDATE posts SET detail=$1 WHERE id=$2`, [detail, id]);
   res.redirect("/profile");
+});
+
+passport.use(
+  new Strategy({ usernameField: "email" }, async function verify(
+    email,
+    password,
+    cb
+  ) {
+    try {
+      if (!email || !password) {
+        return cb(null, false, { message: "Missing credentials" });
+      }
+
+      const result = await db.query("SELECT * FROM users where email= $1", [
+        email,
+      ]);
+      const user = result.rows[0];
+
+      if (!user) {
+        return cb(null, false, { message: "Email not found" });
+      }
+
+      const isValid = await bcrypt.compare(password, user.password);
+      if (!isValid) {
+        return cb(null, false, { message: "Incorrect password" });
+      } else {
+        return cb(null, user);
+      }
+    } catch (err) {
+      return cb(err);
+    }
+  })
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 app.listen(port, (req, res) => {
